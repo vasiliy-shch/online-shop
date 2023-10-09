9bbf973b8ae990609c01c540a11da3cef673576c
vasiliy-shch <thedoomeddie@yandex.ru>
2023-10-09 21:41:25 +0500

    fixed README.md

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'my-nuxt-for-shop',
+    title: 'online-shop',
     htmlAttrs: {
       lang: 'en'
     },
