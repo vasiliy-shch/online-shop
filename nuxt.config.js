b002b23c7cb487cee7a592d54b9162078fabcbe1
vasiliy-shch <thedoomeddie@yandex.ru>
2023-10-12 18:43:02 +0500

    full rework

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'online-shop',
+    title: 'my-nuxt-for-shop',
     htmlAttrs: {
       lang: 'en'
     },
