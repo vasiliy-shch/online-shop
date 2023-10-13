1fa78d55a87c81ebf8a4085aaf30163611611e06
vasiliy-shch <thedoomeddie@yandex.ru>
2023-10-13 19:14:05 +0500

    add layout/default.vue use slot 'header' index.vue

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot name="header">
+        <HeaderShop></HeaderShop>
+    </slot>
+  </div>
+</template>
+<script>
+import HeaderShop from '../components/HeaderShop.vue'
+export default {
+  components: {
+    HeaderShop
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
+    <page slot="header"></page>
   </div>
 </template>
 
 <script>
+import page from '../layouts/default'
 export default {
+  components: {
+    page
+  },
   name: 'IndexPage'
 }
 </script>
